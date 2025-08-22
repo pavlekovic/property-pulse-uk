63a771864185f4c77c3f3c4c5fa605ebe75ca3e0
Dr Renato Pavlekovic <renato.pavlekovic92@gmail.com>
2025-08-22 22:57:15 +0100

    check

--- app/streamlit/pages/price_map.py
+++ app/streamlit/pages/price_map.py
@@ -101,12 +101,12 @@ lookup = dict(zip(sub_nonnull["__norm"], sub_nonnull["avg_price"].round(0)))
 geojson = load_geojson()
 name_field = detect_name_field(geojson, level="Local Authorities")  # Local Authorities, can be changed to postal code
 
-print(name_field)
+#print(name_field)
 
 # Match values from sub (price data) to geojson
 matched_values = attach_values(geojson, lookup, name_field, value_field="avg_price")
 
-print(matched_values)
+#print(matched_values)
 
 # Build quantile scale
 edges, colors = color_scale_quantiles(matched_values, n_bins=6)
