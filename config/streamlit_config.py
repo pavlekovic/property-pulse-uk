d944021c19fb2c6b9e0210d6fd0cd101d55c8679
Dr Renato Pavlekovic <renato.pavlekovic92@gmail.com>
2025-08-23 19:45:23 +0100

    Updated streamlit_config

--- config/streamlit_config.py
+++ config/streamlit_config.py
@@ -10,15 +10,13 @@ MAPPING_DIR = DATA_DIR / "mapping"
 # Data marts
 MART_PREDICTION = MARTS_DIR / "fact_prediction"
 MART_FACT_BY_TYPE = MARTS_DIR / "fact_prices"
-#MART_BOUNDS_5Y = MARTS_DIR / "agg_bounds_5y.parquet"
 
 # Geojson data
 GEOJSON_PATH = MAPPING_DIR / "local_authority.geojson"
 
 # Model path
-MODEL_PATH = BASE_DIR / "models"
-ARTIFACT_PATH = MODEL_PATH / "lintrend_params.pkl"
-#PRED_BASE_PATH = Path("data/marts/fact_prediction")
+MODEL_DIR = BASE_DIR / "models"
+ARTIFACT_PATH = MODEL_DIR / "lintrend_params.pkl"
 
 # Misc
 STREAMLIT_DIR = BASE_DIR / "app" / "streamlit"
